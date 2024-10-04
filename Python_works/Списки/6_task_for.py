1448949b1e4ec98471562964194ed996404e3a55
sky1ex-devops <sky1ex@bk.ru>
2024-10-04 16:16:31 +0300

    r444

---
+++ Python_works/Списки/6_task_for.py
@@ -0,0 +1,10 @@
+# Используем цикл for для поочередного выода значений из списка 
+magics = ['alice', 'devid', 'carolina']
+for magic in magics: # Берем поочередно значение из magics и сохраняем в переменной magic
+    print(f"{magic.title()}, that was a great trick") # выполняем вывод до последнего эоемента в списке
+    print(f"I can't wait to see your next trick, {magic.title()}\n")
+print("Thank you, everyone. That was a great magic show!")
+
+
+ 1
+ 
