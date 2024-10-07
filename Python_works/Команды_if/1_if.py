81c8091ec9cef611309cf2e3fc8ba85a0cdd40ee
sky1ex-devops <sky1ex@bk.ru>
2024-10-07 17:33:36 +0300

    work3465765

--- Python_works/Команды_if/1_if.py
+++ Python_works/Команды_if/1_if.py
@@ -1,4 +1,4 @@
-
+#####################################################################################################################
 # if
 
 req_topping = 'mushroom'
@@ -19,7 +19,7 @@ reqlists = ['mushrooms', 'onions', 'pineapple']
 print(f"\n{'mushrooms' in reqlists}") # true - если значение входит в список
 print("\n")
 
-
+#####################################################################################################################
 # if_else
 
 
@@ -38,9 +38,9 @@ for car in cars:
         print(car.title()) # иначе - выводим только первый символ заглавный
 print("\n")
 
-
+#####################################################################################################################
 # if_elif_else
-
+# для выявления одного совпадения
 
 agee = 49
 
@@ -52,6 +52,10 @@ elif agee > 50:
     print("Сумма к оплате 25р.")
 else:
     print("Сумма к оплате 50р.")
+
+#####################################################################################################################
+# if
+# для выявления нескольких совпадений
 
 print("\n")
 r_topp = ['mushrooms', 'extra', 'cheese']
@@ -65,8 +69,77 @@ if 'papperoni' in r_topp:
 if 'cheese' in r_topp:
     print("Adding cheese")
 
+#####################################################################################################################
+# for and if and lists
+print("\n") 
+requested_toppings = ['mushrooms', 'green peppers', 'extra chees']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
+
+#####################################################################################################################
 
 print("\n")
-alien_color = ['green', 'yellow', 'red']
-if 'white' in alien_color:
-    print("Вы выбрали green - заработали 5 очков")
+requested_toppings = [] # Пустой список
+if requested_toppings: # Если ..
+    for requested_topping in requested_toppings:
+        print(f"Добавлен {requested_topping}.") # Имеется значение в переменной
+    print("\nВаши пожелания исполнены!") # выводим сообщение
+else: # если нет значения
+    print("\nИнградиенты закончились") # выводим это сообщение
+
+#####################################################################################################################
+# Множественные списки
+
+print("\n")
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'papperoni', 'extra cheese', 'pineapple'] # Список возможных добавок
+requested_toppings = ['mushrooms',  'papperoni', 'extra cheese', 'tee', 'coffe'] # Добавки которые заазал клиент
+
+for requested_topping in requested_toppings: 
+    if requested_topping in available_toppings: # если значиения из requested_topping имеються в available_toppings, то ..
+        print(f"Adding {requested_topping}.") # выводим заказаные добавки
+    else:                                     # если условия выше не совпали...
+        print(f"Sorry, we don`t have {requested_topping}.") #Показываем какого инградиента не хватает
+print("\nFinished making your pizza")
+
+#####################################################################################################################
+print("\n")
+who = 'usr' # ввод пользователя 
+users = ['usr',  'Admin', 'lui', 'jora', 'Djon_Doe'] # имеющийся пользователи
+
+if "admin" in who:                                   # если ввели admin 
+    print(f"Привет {who}.")                          # выводим привет admin
+elif who in users:                                   # если ввели пользователя из списка имеющихся
+    print(f"Привет {who}")                           # выводим привет по имени
+else:                                                # если ввели не из имеющихся и не admin
+    who != users and who != 'Admin'                  # ввыводим сообщение ....
+    print("Спасибо что посетили наш сайт")
+
+#####################################################################################################################
+print("\n")
+new_users = ['Djin',  'Admin', 'lui', 'Petr', 'Gosha'] # новые обтекты 
+current_users = ['usr',  'Admin', 'lui', 'jora', 'Djon_Doe'] # имеющейся база
+
+for new_user in new_users: # заставляем применять нижесказанное к каждому элементу в списке new_users
+    if new_user in current_users: # если новый обьект имееться в базе (current_users)
+        print(f"\nПользователь '{new_user}' уже существует в базе, придумайте другой Ник") # выводим сообщение 
+    else: # если
+        new_user != current_users # нет совпадения нового обьекта с базой
+        print(f"\nПользователь {new_user} создан!") # выводим сообщение
+
+#####################################################################################################################
+print("\n")
+nmbrs = list(range(1,10))
+for nmbr in nmbrs:
+    if nmbr >= 4: 
+        print(f"{nmbr}th")
+    elif nmbr == 3:
+        print(f"{nmbr}rd")
+    elif nmbr == 2:
+        print(f"{nmbr}nd")
+    elif nmbr == 1:
+        print(f"{nmbr}st")
